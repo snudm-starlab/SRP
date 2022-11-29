c236c29c0b79860c7d4e9006309b8c071bcafdba
ant6si <ant6si@snu.ac.kr>
2022-11-29 08:22:27 +0000

    cleaning code

--- script/iwslt_preprocess.sh
+++
@@ -1,9 +0,0 @@
-# Download and prepare the data
-bash prepare-iwslt14.sh
-
-# Preprocess/binarize the data
-TEXT=../data-bin/iwslt14.tokenized.de-en
-fairseq-preprocess --source-lang de --target-lang en \
-    --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test \
-    --destdir ../data-bin/iwslt14.tokenized.de-en \
-    --workers 20
